16eea8b25dbf8b3ca581d7fbcd9086468c092220
Lee Junmo <ezurno@gmail.com>
2022-06-29 15:08:28 +0900

    day 3
    1. 롬복을 사용해 리팩토링
    2. 코드간결화 라이브러리
    3. @Slf4j, @AllArgsConstructor

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -2,12 +2,18 @@ package com.example.firstproject.dto;
 //form data를 받아올 그릇
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+
+@AllArgsConstructor // this. 함수 대신 리팩토리 어노테이션
+@ToString // toString 리팩토리 어노테이션
 public class ArticleForm {
 
     private String title;
     private String content;
 
+    /*
     public ArticleForm(String title, String content) {
         this.title = title;
         this.content = content;
@@ -19,7 +25,7 @@ public class ArticleForm {
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
-    }
+    }*/
 
     public Article toEntity() {
         return new Article(null, title, content);
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,17 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
 @Entity
+@AllArgsConstructor // this.함수 대신 어노테이션
+@ToString // toString 함수 대신 어노테이션
 public class Article {
 
     @Id // 대푯값을 지정 like a 주민등록번호
@@ -18,6 +24,7 @@ public class Article {
     @Column
     private String content;
 
+    /*
     public Article(Long id, String title, String content) {
         this.id = id;
         this.title = title;
@@ -31,5 +38,5 @@ public class Article {
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
-    }
+    }*/
 }
